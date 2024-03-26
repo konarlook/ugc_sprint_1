72280287be799ca2832de45e65b553e8e7f9fca5
konarlook <usikov.dv@gmail.com>
2024-03-26 23:46:02 +0300

    feat(16_add_base_flask):add base dirs

--- ugc/src/main.py
+++ ugc/src/main.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_swagger_ui import get_swaggerui_blueprint
 from api.v1.events import routers
 
-app = Flask(__name__)
-
 SWAGGER_URL = "/ugc/api/openapi"
 API_URL = "/static/api/v1/openapi.yaml"
 
@@ -15,8 +13,17 @@ swagger_blueprint = get_swaggerui_blueprint(
     },
 )
 
-app.register_blueprint(swagger_blueprint)
-app.register_blueprint(routers)
+
+def create_app():
+    flask_application = Flask(__name__)
+
+    flask_application.register_blueprint(swagger_blueprint)
+    flask_application.register_blueprint(routers)
+
+    return flask_application
+
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
