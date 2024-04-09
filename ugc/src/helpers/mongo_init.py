f981e524b511e8a5c6d1e376180967256a841d1b
MosyaginGrigorii <mosyagin.grisha@gmail.com>
2024-04-10 01:00:55 +0300

    fix(ugc): Добавлены модели коллекций в инициализацию mongo

--- ugc/src/helpers/mongo_init.py
+++ ugc/src/helpers/mongo_init.py
@@ -1,19 +1,12 @@
-from typing import Optional
-
-from beanie import Document, Indexed, init_beanie
+from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from core.config import settings
 from db.mongo import get_mongo_client
 from helpers import logger
+from models.mongo.collections import Review, ReviewRating, Bookmark
 
 mongo_logger = logger.UGCLogger()
-
-
-class Product(Document):
-    name: str
-    description: Optional[str] = None
-    price: Indexed(float)
 
 
 class MongoDBInit:
@@ -22,7 +15,7 @@ class MongoDBInit:
 
     async def create_collections(self):
         self.client.db_name = settings.mongodb.mongodb_db_name
-        await init_beanie(database=self.client.db_name, document_models=[Product])
+        await init_beanie(database=self.client.db_name, document_models=[Review, Bookmark, ReviewRating])
 
 
 def get_mongodb_init() -> MongoDBInit:
