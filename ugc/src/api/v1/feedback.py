3a810ea8859d9d08bf4b1c1f503f947c8dab1adb
konarlook <usikov.dv@gmail.com>
2024-04-16 01:36:02 +0300

    feat(52-add_review_ugc):add delete_reviews api

--- ugc/src/api/v1/feedback.py
+++ ugc/src/api/v1/feedback.py
@@ -56,6 +56,24 @@ async def get_reviews(
     return Response(json.dumps(response, default=str), mimetype="application/json")
 
 
+@router.route("/review_admin", methods=["DELETE"])
+@inject
+@check_access_token
+async def admin_delete_review(
+    user_info: dict = None,
+    review_service: ReviewService = Depends(get_review_service),
+):
+    """Delete review by admin on film-work."""
+    request_data: dict = request.args.to_dict()
+    await review_service.delete(
+        document={
+            "user_id": str(request_data.get("user_id")),
+            "movie_id": str(request_data.get("movie_id")),
+        }
+    )
+    return jsonify({"message": "Successful deleting"}), HTTPStatus.OK
+
+
 @router.route("/review", methods=["DELETE"])
 @inject
 @check_access_token
@@ -63,8 +81,18 @@ async def delete_review(
     user_info: dict = None,
     review_service: ReviewService = Depends(get_review_service),
 ):
-    """Delete self review on film-work."""
-    pass
+    """Update self review on film-work."""
+    request_data: dict = request.args.to_dict()
+    await review_service.update(
+        filter_data={
+            "user_id": str(user_info.get("sub")),
+            "movie_id": str(request_data.get("movie_id")),
+        },
+        update_data={
+            "is_delete": True,
+        },
+    )
+    return jsonify({"message": "Successful writing"}), HTTPStatus.OK
 
 
 @router.route("/review", methods=["UPDATE"])
